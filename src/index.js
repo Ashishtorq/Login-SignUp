a0a416259809ee21eefaa7b60ac0028b20db626c
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-23 17:22:55 +0530

    path assigned

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const port = 4500;
+const hbs = require("hbs");
+const templatePath = path.join(__dirname, "../templates");
+
+app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("/signup");
+});
+
+
+
 app.listen(port, (req, res) => {
   console.log(`server is running on  Port ${port}`);
 });
