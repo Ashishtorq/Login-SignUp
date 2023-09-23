69cc61b10b8029a9d4998e72684582bd727dca85
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-23 17:12:05 +0530

    port 4500

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-const 
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 4500;
+app.listen(port, (req, res) => {
+  console.log(`server is running on  Port ${port}`);
+});
