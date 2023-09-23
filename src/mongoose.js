d438c4a570322ea09e0bd9b14bf8e4b4ffe698f8
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-23 17:40:48 +0530

    database connected

---
+++ src/mongoose.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb:localhost:27017/loginPage")
+
+.then(()=>{
+    console.log("Database Connected Successfully")
+})
+.catch((e)=>{
+    console.log(e);
+})
+
+const LogInSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = new mongoose.model("Collection1",LogInSchema);
+
+module.exports = collection;
