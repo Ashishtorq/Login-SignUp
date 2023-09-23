382e2dc6367d46bdc7a61612f63aa53dd9685294
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-23 18:52:58 +0530

    bug

--- src/index.js
+++ src/index.js
@@ -3,18 +3,38 @@ const app = express();
 const path = require("path");
 const port = 4500;
 const hbs = require("hbs");
+const async = require("hbs/lib/async");
+const collection = require("./mongoose");
+
 const templatePath = path.join(__dirname, "../templates");
 
 app.use(express.json());
 app.set("view engine", "hbs");
 app.set("views", templatePath);
+app.use(express.urlencoded({extended:false}))
+
+
 
 
 app.get("/", (req, res) => {
   res.render("login");
 });
+
+
 app.get("/signup", (req, res) => {
   res.render("/signup");
+});
+
+
+
+app.post("/signup", async(req, res) => {
+  const data = {
+    name:req.body.name,
+    password:req.body.password
+  }
+  // data to database
+  await collection.insertMany([data]);
+  res.render("home")
 });
 
 
